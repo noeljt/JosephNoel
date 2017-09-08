94e59de41096b8dc5b68e9cef402cca63d2323f2
Joseph Noel <jtnoel96@gmail.com>
2017-09-08 14:21:23 -0400

    Set up heroku

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app);
+var express = require('express');
+var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', (process.env.PORT || 5000));
 
-  server.listen(5000, function () {
-    console.log('Quiz 2 listening on port 8000...');
-  });
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
