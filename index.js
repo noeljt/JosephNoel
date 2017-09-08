1222b27592ff0a2cc623c0148eb37dacea251bd6
Joseph Noel <jtnoel96@gmail.com>
2017-09-08 16:15:27 -0400

    Fixed node server to serve bootstrap files properly

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use('/js',  express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js',  express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/js',  express.static(__dirname + '/node_modules/popper.js/dist/umd')); // redirect popper.js
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/css', express.static(__dirname + '/node_modules/font-awesome/css')); // redirect font-awesome css
+app.use('/fonts', express.static('./node_modules/font-awesome/fonts')) // redirect font-awesome fonts
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
